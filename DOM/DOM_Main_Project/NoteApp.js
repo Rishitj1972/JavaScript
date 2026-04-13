361600ebbcdb8619d0da059393422f71cf350836
Rishi <rishitj1972@gmail.com>
2026-04-13 08:49:23 +0530

    DOM project 2 finished

---
+++ DOM/DOM_Main_Project/NoteApp.js
@@ -0,0 +1,74 @@
+let input = document.getElementById("noteInput");
+let add = document.getElementById("addBtn");
+let clear = document.getElementById("clearBtn");
+let counter = document.getElementById("count");
+let container = document.getElementById("noteContainer");
+
+let count = 0;
+
+function updateCount() {
+    counter.innerText=count;
+}
+
+function addNote() {
+
+    // get the input
+    const text = input.value.trim();
+    if(text == "") {
+        alert("Input Cannot Be Empty");
+        return;
+    }
+
+    // create Note
+    const note = document.createElement("div");
+    note.classList.add("note");
+
+    // text span
+    const span = document.createElement("span");
+    span.innerText = text;
+
+    // button container
+    const btnGroup = document.createElement("div");
+
+    // Delete Button
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+
+    delBtn.addEventListener("click", function(){
+        container.removeChild(note);    
+        count--;
+        updateCount();
+    });
+
+    const impBtn = document.createElement("button");
+    impBtn.innerText = "important";
+
+    impBtn.addEventListener("click", function() {
+        note.classList.toggle("important");
+    })
+
+    btnGroup.appendChild(impBtn);
+    btnGroup.appendChild(delBtn);
+
+    note.appendChild(span);
+    note.appendChild(btnGroup);
+
+    container.appendChild(note);
+
+    count++;
+    updateCount();
+
+    input.value="";
+}
+
+add.addEventListener("click", addNote);
+
+input.addEventListener("keypress", function(e) {
+    if(e.key == "Enter") addNote();
+});
+
+clear.addEventListener("click", function() {
+    container.innerText= "";
+    count = 0;
+    updateCount();
+})
